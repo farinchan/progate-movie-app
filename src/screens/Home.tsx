3afaef06dc3693580e8df6756c76dae874a1e271
fajri <fajri@gariskode.com>
2024-07-06 09:44:13 +0700

    set up environment variables

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,24 +1,23 @@
 import React from 'react'
 import { Button, StyleSheet, Text, View } from 'react-native'
 
-const Home = ({ navigation }: any): JSX.Element => {
-    return (
-        <View style={styles.container}>
-            <Text>Home</Text>
-            <Button title='Pindah Halaman' onPress={() =>
-                navigation.navigate('MovieDetail')
-            } />
-        </View>
-
-    )
+const Home = ({navigation}:any):JSX.Element => {
+  return (
+    <View style={styles.container}>
+        <Text>Home</Text>
+        <Button title='Pindah Halaman' onPress={()=>
+          navigation.navigate('MovieDetail') 
+      }/>
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: "center",
-        justifyContent: "center"
-    },
+  container:{
+    flex:1,
+    alignItems:"center",
+    justifyContent:"center"
+},
 })
 
 
